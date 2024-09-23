f586cab6b35bcee468862e339edbb2f4f292e837
ahmed hassan <a7med.abdo6@gmail.com>
2024-09-23 11:35:07 +0300

    fic

--- src/lib/features/categoriesSlice.ts
+++ src/lib/features/categoriesSlice.ts
@@ -1,44 +1,57 @@
-import { fetchCategories } from '@/services/api';
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
+import { fetchCategories } from "@/services/api";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface CategoriesState {
   data: any; // Replace `any` with the appropriate type for your categories data
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  status: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
 const initialState: CategoriesState = {
-  data: JSON.parse(localStorage.getItem('categoriesData') || 'null'),
-  status: 'idle',
+  data:
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage?.getItem("categoriesData") || "null")
+      : null,
+  status: "idle",
   error: null,
 };
 
 // Thunk to fetch categories
-export const fetchCategoriesData = createAsyncThunk('categories/fetchCategoriesData', async (lng: string) => {
-  const response = await fetchCategories(lng);
-  console.log(response);
+export const fetchCategoriesData = createAsyncThunk(
+  "categories/fetchCategoriesData",
+  async (lng: string) => {
+    const response = await fetchCategories(lng);
+    console.log(response);
 
-  return response;
-});
+    return response;
+  }
+);
 
 export const categoriesSlice = createSlice({
-  name: 'categories',
+  name: "categories",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchCategoriesData.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
-      .addCase(fetchCategoriesData.fulfilled, (state, action: PayloadAction<any>) => { // Replace `any` with your data type
-        state.status = 'succeeded';
-        state.data = action.payload;
-        localStorage.setItem('categoriesData', JSON.stringify(action.payload));
-      })
+      .addCase(
+        fetchCategoriesData.fulfilled,
+        (state, action: PayloadAction<any>) => {
+          // Replace `any` with your data type
+          state.status = "succeeded";
+          state.data = action.payload;
+          localStorage.setItem(
+            "categoriesData",
+            JSON.stringify(action.payload)
+          );
+        }
+      )
       .addCase(fetchCategoriesData.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message || 'Failed to fetch categories';
+        state.status = "failed";
+        state.error = action.error.message || "Failed to fetch categories";
       });
   },
 });
--- src/lib/features/homeSlice.ts
+++ src/lib/features/homeSlice.ts
@@ -1,42 +1,48 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
-import { fetchHome } from '../../services/api'; // Assuming your fetchHome function is exported from api file
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { fetchHome } from "../../services/api"; // Assuming your fetchHome function is exported from api file
 
 interface HomeState {
   data: any; // Replace `any` with the appropriate type for your data
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  status: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
 const initialState: HomeState = {
-  data: JSON.parse(localStorage.getItem('homeData') || 'null'),
-  status: 'idle',
+  data:
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage?.getItem("homeData") || "null")
+      : null,
+  status: "idle",
   error: null,
 };
 
-export const fetchHomeData = createAsyncThunk('home/fetchHomeData', async (lng: string)  => {
-
-  const response = await fetchHome(lng);
-  return response;
-});
+export const fetchHomeData = createAsyncThunk(
+  "home/fetchHomeData",
+  async (lng: string) => {
+    const response = await fetchHome(lng);
+    return response;
+  }
+);
 
 export const homeSlice = createSlice({
-  name: 'home',
+  name: "home",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchHomeData.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
-      .addCase(fetchHomeData.fulfilled, (state, action: PayloadAction<any>) => { // Replace `any` with your data type
-        state.status = 'succeeded';
+      .addCase(fetchHomeData.fulfilled, (state, action: PayloadAction<any>) => {
+        // Replace `any` with your data type
+        state.status = "succeeded";
         state.data = action.payload;
-        localStorage.setItem('homeData', JSON.stringify(action.payload));
+        localStorage.setItem("homeData", JSON.stringify(action.payload));
       })
       .addCase(fetchHomeData.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message || 'Failed to fetch data';
+        state.status = "failed";
+        state.error = action.error.message || "Failed to fetch data";
       });
   },
 });
