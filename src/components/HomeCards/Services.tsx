1f8087fb6a894c29314f9dd995659ad3881dace0
ahmed hassan <a7med.abdo6@gmail.com>
2024-12-23 22:23:18 +0200

    f

--- src/components/HomeCards/Services.tsx
+++ src/components/HomeCards/Services.tsx
@@ -33,87 +33,91 @@ const Services: React.FC<{ dataService: any }> = ({ dataService }) => {
         paddingBottom: { xs: "10px", sm: "20px", md: "20px" },
       }}
     >
-      <Typography
-        variant="h4"
-        color="#476B87"
-        gutterBottom
-        mt={10}
-        sx={{
-          textAlign: "center",
+      {dataService && (
+        <>
+          <Typography
+            variant="h4"
+            color="#476B87"
+            gutterBottom
+            mt={10}
+            sx={{
+              textAlign: "center",
 
-          paddingRight: { xs: "10px", sm: "20px", md: "40px" },
-          paddingLeft: { xs: "10px", sm: "20px", md: "40px" },
-        }}
-      >
-        {dataService?.f_title}{" "}
-        <Box component="span" color="#C99700">
-          {dataService?.s_title}{" "}
-        </Box>
-        {dataService?.t_title}{" "}
-      </Typography>
-      <Typography
-        variant="body1"
-        color="#606060"
-        mb={4}
-        sx={{
-          textAlign: "center",
-
-          paddingRight: { xs: "10px", sm: "20px", md: "100px" },
-          paddingLeft: { xs: "10px", sm: "20px", md: "100px" },
-        }}
-      >
-        {dataService?.description}{" "}
-      </Typography>
-      <Grid container spacing={3} justifyContent="center">
-        {dataService?.items?.map(
-          (
-            card: { icon: string; title: string; description: string },
-            index: React.Key | null | undefined
-          ) => (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              md={4}
-              key={index}
-              style={{ cursor: "pointer" }}
-              onClick={() => handleOpenService(card.description)}
-            >
-              <CustomCard
-                imageSrc={card.icon}
-                title={card.title}
-                description={card.description}
-              />
-            </Grid>
-          )
-        )}
-      </Grid>
-      <Link href={`/${pathAfterSlash}/whoarewe`} passHref>
-        <Box
-          sx={{
-            paddingTop: { xs: "10px", sm: "10px", md: "20px" },
-          }}
-        >
-          <CustomButton
-            onClick={handleClick}
-            customColor="white"
-            width="175px"
-            height="48px"
-            backgroundColor={colors.active}
-            borderRadius="4px"
+              paddingRight: { xs: "10px", sm: "20px", md: "40px" },
+              paddingLeft: { xs: "10px", sm: "20px", md: "40px" },
+            }}
           >
-            {t("contactUs")}
-          </CustomButton>
-        </Box>
-      </Link>
+            {dataService?.f_title}{" "}
+            <Box component="span" color="#C99700">
+              {dataService?.s_title}{" "}
+            </Box>
+            {dataService?.t_title}{" "}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="#606060"
+            mb={4}
+            sx={{
+              textAlign: "center",
 
-      <Dialog onClose={handleClose} open={open}>
-        <p
-          dangerouslySetInnerHTML={{ __html: details }}
-          color="#555"
-          style={{ padding: "30px", width: "400px", textAlign: "center" }}
-        ></p>
-      </Dialog>
+              paddingRight: { xs: "10px", sm: "20px", md: "100px" },
+              paddingLeft: { xs: "10px", sm: "20px", md: "100px" },
+            }}
+          >
+            {dataService?.description}{" "}
+          </Typography>
+          <Grid container spacing={3} justifyContent="center">
+            {dataService?.items?.map(
+              (
+                card: { icon: string; title: string; description: string },
+                index: React.Key | null | undefined
+              ) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  key={index}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleOpenService(card.description)}
+                >
+                  <CustomCard
+                    imageSrc={card.icon}
+                    title={card.title}
+                    description={card.description}
+                  />
+                </Grid>
+              )
+            )}
+          </Grid>
+          <Link href={`/${pathAfterSlash}/whoarewe`} passHref>
+            <Box
+              sx={{
+                paddingTop: { xs: "10px", sm: "10px", md: "20px" },
+              }}
+            >
+              <CustomButton
+                onClick={handleClick}
+                customColor="white"
+                width="175px"
+                height="48px"
+                backgroundColor={colors.active}
+                borderRadius="4px"
+              >
+                {t("contactUs")}
+              </CustomButton>
+            </Box>
+          </Link>
+
+          <Dialog onClose={handleClose} open={open}>
+            <p
+              dangerouslySetInnerHTML={{ __html: details }}
+              color="#555"
+              style={{ padding: "30px", width: "400px", textAlign: "center" }}
+            ></p>
+          </Dialog>
+        </>
+      )}
     </Box>
   );
 };
